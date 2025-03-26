a6e10620874f399620362154b43b487cb34af985
ayscd <ayeskacsta@gmail.com>
2025-03-26 19:09:05 -0300

    Update-5
    - importing real weather description from API
    - importing real humidity from API
    - importing real wind speed from API
    - importing real date and time from API trough parse

--- src/app.js
+++ src/app.js
@@ -1,17 +1,40 @@
-function updateTemp(info) {
+function displayDate(time) {
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[time.getDay()];
+  let minutes = time.getMinutes();
+  let hours = time.getHours();
+
+  if (minutes < 10) {
+     minutes = `0${minutes}`;
+    }
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
+
+function updateInfo(info) {
   let temp = document.querySelector("#real-temp");
   let realTemp = info.data.temperature.current;
   let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let date = document.querySelector("#date");
+  let time = new Date(info.data.time * 1000);
 
-  city.innerHTML = info.data.city;
   temp.innerHTML = Math.round(realTemp);
+  city.innerHTML = info.data.city;
+  description.innerHTML = info.data.condition.description;
+  humidity.innerHTML = `${info.data.temperature.humidity}%`;
+  wind.innerHTML = `${info.data.wind.speed}km/h`;
+  date.innerHTML = displayDate(time);
 }
 
 function searchInfo(city) {
     let apiKey = "do8ae0f5a33a8b61b41f1t1ed44678b4";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     
-    axios.get(apiUrl).then(updateTemp);
+    axios.get(apiUrl).then(updateInfo);
 }
 
 function handleSubmit(event) {
