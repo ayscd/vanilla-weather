9a08a29c7e72b096504f2e7223f531c33a74c3c4
ayscd <ayeskacsta@gmail.com>
2025-03-26 18:28:07 -0300

    Update-4
    - importing axios
    - connecting the API key
    - importing city data from API
    - importing city temp from API
    - defining a default city for the app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
+function updateTemp(info) {
+  let temp = document.querySelector("#real-temp");
+  let realTemp = info.data.temperature.current;
+  let city = document.querySelector("#city");
+
+  city.innerHTML = info.data.city;
+  temp.innerHTML = Math.round(realTemp);
+}
+
+function searchInfo(city) {
+    let apiKey = "do8ae0f5a33a8b61b41f1t1ed44678b4";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(updateTemp);
+}
+
 function handleSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
-    let city = document.querySelector("#city");
-    city.innerHTML = searchInput.value;
 
-    //still needs to add link to API
+    searchInfo(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
+
+searchInfo("Belo Horizonte");
